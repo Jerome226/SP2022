bb1a906593c5084abce87e1b9f208ca95bdc63d2
Jerome226 <55724525+Jerome226@users.noreply.github.com>
2022-05-19 00:01:30 -0400

    Second commit

--- src/com/fuguclub/sp2022/Main.java
+++ src/com/fuguclub/sp2022/Main.java
@@ -1,5 +1,6 @@
 package com.fuguclub.sp2022;
 
+import java.awt.*;
 import java.util.Random;
 
 public class Main {
@@ -11,14 +12,20 @@ public class Main {
     public static int firstBandColor;
     public static int secondBandColor;
     public static int multiplierBandColor;
+    public static int multiplier;
+    public static Color multiplierColor;
     public static int toleranceBandColor;
+    public static String tolerance;
+    public static Color toleranceColor;
     public static int randomizedAnswerOne;
     public static int randomizedAnswerSecond;
 
     public static void main(String[] args) {
+    System.out.println("init: Main");
 
     Random rand = new Random();
-    answerButton = rand.nextInt((3 - 1) + 1) + 1;
+    answerButton = rand.nextInt((5 - 1) + 1) + 1;
+
     firstBandColor = rand.nextInt((9 - 1) + 1) + 1;
     secondBandColor = rand.nextInt((9 - 1) + 1) + 1;
     multiplierBandColor = rand.nextInt((9 - 1) + 1) + 1;
@@ -27,15 +34,83 @@ public class Main {
     randomizedAnswerOne = rand.nextInt((24000 - 1) + 1) + 1;
     randomizedAnswerSecond = rand.nextInt((24000 - 1) + 1) + 1;
 
-    int answer = 10;
+    switch(multiplierBandColor) {
+        case 1:
+            multiplier = 1;
+            multiplierColor = Color.black;
+        break;
+        case 2:
+            multiplier = 10;
+            multiplierColor = new Color(69, 42, 14);
+        break;
+        case 3:
+            multiplier = 100;
+            multiplierColor = Color.red;
+        break;
+        case 4:
+            multiplier = 1000;
+            multiplierColor = Color.orange;
+        break;
+        case 5:
+            multiplier = 10000;
+            multiplierColor = Color.yellow;
+        break;
+        case 6:
+            multiplier = 100000;
+            multiplierColor = Color.green;
+        break;
+        case 7:
+            multiplier = 1000000;
+            multiplierColor = Color.blue;
+        break;
+        case 8:
+            multiplier = 10000000;
+            multiplierColor = new Color(218, 66, 245);
+        break;
+        case 9:
+            multiplier = 100000000;
+            multiplierColor = Color.gray;
+        break;
+        case 10:
+            multiplier = 1000000000;
+            multiplierColor = Color.white;
+        break;
+    }
+
+    switch(toleranceBandColor) {
+        case 1:
+            tolerance = "∓5%";
+            toleranceColor = new Color(209, 138, 31);
+        break;
+        case 2:
+            tolerance = "∓10%";
+            toleranceColor = new Color(145, 143, 140);
+        break;
+        case 3:
+            tolerance = "∓20%";
+            toleranceColor = Color.black;
+        break;
+    }
+
+    int first = 1;
+    int second = 2;
+
+    String firstText = String.valueOf(first);
+    String secondText = String.valueOf(second);
+
+    String numberText = firstText + secondText;
+    int number = Integer.parseInt(numberText);
+    int answer = number * multiplier;
+    answerText = answer + System.lineSeparator() + "(Tolérance: " + tolerance + " )";
 
     randomizedAnswerOneText = String.valueOf(randomizedAnswerOne);
     randomizedAnswerSecondText = String.valueOf(randomizedAnswerSecond);
 
-    answerText = String.valueOf(answer);
+    //
 
     //Frame
     MainFrame frame = new MainFrame();
+
     }
 
 }
--- src/com/fuguclub/sp2022/MainFrame.java
+++ src/com/fuguclub/sp2022/MainFrame.java
@@ -3,23 +3,36 @@ package com.fuguclub.sp2022;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 import static com.fuguclub.sp2022.Main.*;
 
-public class MainFrame extends JFrame implements ActionListener {
+public class MainFrame extends JFrame implements ActionListener{
 
     JButton firstButton;
     JButton secondButton;
     JButton thirdButton;
+    JButton fourthButton;
+    JButton fifthButton;
+    JPanel panel;
+    JPanel panelCenter;
+    JPanel panelEast;
+    JPanel panelWest;
+    JPanel panelBottom;
+    JPanel panelBottomBottom;
+    JPanel panelBottomCenter;
+    JPanel panelBottomEast;
+    JPanel panelBottomWest;
     JLabel label;
+    JLabel labelBottom;
+    //JLabel labelBottom;
     JLabel answerLabel;
 
     public void rightAnswer(){
+        System.out.println("init: rightAnswer");
+
         ImageIcon checkmark = new ImageIcon("checkmark.png");
 
-        answerLabel = new JLabel();
         answerLabel.setText("Bonne réponse! Bravo");
         answerLabel.setIcon(checkmark);
         answerLabel.setIconTextGap(40);
@@ -27,21 +40,45 @@ public class MainFrame extends JFrame implements ActionListener {
     }
 
     public void wrongAnswer(){
+        System.out.println("init: wrongAnswer");
+
+        ImageIcon wrong = new ImageIcon("wrong.png");
+
+        answerLabel.setText("Mauvaise réponse! Ré-essai");
+        answerLabel.setIcon(wrong);
+        answerLabel.setIconTextGap(40);
+        answerLabel.setVisible(true);
+    }
+
+    public void generateWindowSize(){
+
+        int frameWidth = getWidth();
+        int buttonBorderWidth = frameWidth / 5;
+
+        panelBottomEast.setPreferredSize(new Dimension(buttonBorderWidth, 100));
+        panelBottomWest.setPreferredSize(new Dimension(buttonBorderWidth, 100));
+        panelEast.setPreferredSize(new Dimension(buttonBorderWidth, 100));
+        panelWest.setPreferredSize(new Dimension(buttonBorderWidth, 100));
 
     }
 
     MainFrame(){
+    System.out.println("init: MainFrame");
 
-    ImageIcon icon = new ImageIcon("icon.png");
-    ImageIcon wrong = new ImageIcon("wrong.png");
+    ImageIcon iconImage = new ImageIcon("icon.png");
+    Image icon2 = iconImage.getImage();
+    Image iconResized = icon2.getScaledInstance(120, 120, java.awt.Image.SCALE_SMOOTH);
+    ImageIcon icon = new ImageIcon(iconResized);
 
-    Border border = BorderFactory.createLineBorder(Color.BLACK,20,true);
+    Border border = BorderFactory.createLineBorder(Color.BLACK,10,false);
     //System.out.println(randomNum2);
 
     //Title
     label = new JLabel();
     label.setText("Calculateur de Resistance");
     label.setIcon(icon);
+    label.setSize(new Dimension(100, 100));
+    //label.setToolTipText("test");
     label.setHorizontalTextPosition(JLabel.RIGHT);
     //label.setVerticalTextPosition(JLabel.)
     label.setHorizontalAlignment(JLabel.CENTER);
@@ -49,61 +86,164 @@ public class MainFrame extends JFrame implements ActionListener {
     label.setFont(new Font("MV Boli",Font.BOLD,40));
     label.setIconTextGap(40);
     label.setVisible(true);
-    label.setBorder(border);
     //label.setBorder(border);
-        
+    label.setPreferredSize(new Dimension(300, 100));
+
+    labelBottom = new JLabel();
+    labelBottom.setText("Choisi la bonne réponse");
+    labelBottom.setFont(new Font("MV Boli",Font.BOLD,20));
+    labelBottom.setVerticalAlignment(JLabel.TOP);
+    labelBottom.setHorizontalAlignment(JLabel.CENTER);
+    labelBottom.setVerticalTextPosition(JLabel.TOP);
+    labelBottom.setPreferredSize(new Dimension(10, 50));
+    labelBottom.setVisible(true);
+
+    answerLabel = new JLabel();
+
     firstButton = new JButton();
     //firstButton.setText(answerText);
     firstButton.setFocusable(false);
-    firstButton.setVisible(true);
-    firstButton.setBounds(100, 100, 100, 50);
     firstButton.addActionListener(this);
+    firstButton.setVisible(true);
 
     secondButton = new JButton();
-    //firstButton.setText(answerText);
+    //secondButton.setText(answerText);
     secondButton.setFocusable(false);
-    secondButton.setVisible(true);
-    secondButton.setBounds(200, 100, 100, 50);
     secondButton.addActionListener(this);
+    secondButton.setVisible(true);
 
     thirdButton = new JButton();
-    //firstButton.setText(answerText);
+    //thirdButton.setText(answerText);
     thirdButton.setFocusable(false);
-    thirdButton.setVisible(true);
-    thirdButton.setBounds(200, 100, 100, 50);
     thirdButton.addActionListener(this);
+    thirdButton.setVisible(true);
+
+    fourthButton = new JButton();
+    //thirdButton.setText(answerText);
+    fourthButton.setFocusable(false);
+    fourthButton.addActionListener(this);
+    fourthButton.setVisible(true);
+
+    fifthButton = new JButton();
+    //thirdButton.setText(answerText);
+    fifthButton.setFocusable(false);
+    fifthButton.addActionListener(this);
+    fifthButton.setVisible(true);
 
     switch(answerButton) {
         case 1:
             firstButton.setText(answerText);
-            secondButton.setText(randomizedAnswerOneText);
-            thirdButton.setText(randomizedAnswerSecondText);
+            secondButton.setText("dummy");
+            thirdButton.setText("dummy");
+            fourthButton.setText("dummy");
+            fifthButton.setText("dummy");
         break;
         case 2:
-            firstButton.setText(randomizedAnswerSecondText);
+            firstButton.setText("dummy");
             secondButton.setText(answerText);
-            thirdButton.setText(randomizedAnswerOneText);
+            thirdButton.setText("dummy");
+            fourthButton.setText("dummy");
+            fifthButton.setText("dummy");
         break;
         case 3:
-            firstButton.setText(randomizedAnswerOneText);
-            secondButton.setText(randomizedAnswerSecondText);
+            firstButton.setText("dummy");
+            secondButton.setText("dummy");
             thirdButton.setText(answerText);
+            fourthButton.setText("dummy");
+            fifthButton.setText("dummy");
+        break;
+        case 4:
+            firstButton.setText("dummy");
+            secondButton.setText("dummy");
+            thirdButton.setText("dummy");
+            fourthButton.setText(answerText);
+            fifthButton.setText("dummy");
+        break;
+        case 5:
+            firstButton.setText("dummy");
+            secondButton.setText("dummy");
+            thirdButton.setText("dummy");
+            fourthButton.setText("dummy");
+            fifthButton.setText(answerText);
         break;
     }
 
     //GUI
     this.setTitle("School Project 2022");
-    //this.setLayout(null);
+    this.setLayout(new BorderLayout());
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
     this.setResizable(true);
-    this.setVisible(true);
     this.setSize(1000,1000);
     this.setIconImage(icon.getImage());
     this.getContentPane().setBackground(Color.WHITE);
-    this.add(firstButton);
-    this.add(secondButton);
-    this.add(thirdButton);
-    this.add(label);
+
+    panelBottomCenter = new JPanel(new GridLayout(1, 5, 20, 20));
+    panelBottomCenter.add(firstButton);
+    panelBottomCenter.add(secondButton);
+    panelBottomCenter.add(thirdButton);
+    panelBottomCenter.add(fourthButton);
+    panelBottomCenter.add(fifthButton);
+
+    panelBottomBottom = new JPanel(new BorderLayout());
+    panelBottomBottom.setPreferredSize(new Dimension(100, 50));
+    panelBottomEast = new JPanel(new BorderLayout());
+    panelBottomWest = new JPanel(new BorderLayout());
+
+    panelBottom = new JPanel(new BorderLayout());
+    panelBottom.setPreferredSize(new Dimension(50, 200));
+    panelBottom.setBorder(border);
+    panelBottom.add(labelBottom,BorderLayout.NORTH);
+    //panelBottom.add(secondButton,BorderLayout.CENTER);
+    panelBottom.add(panelBottomCenter,BorderLayout.CENTER);
+    panelBottom.add(panelBottomBottom,BorderLayout.SOUTH);
+    panelBottom.add(panelBottomEast,BorderLayout.EAST);
+    panelBottom.add(panelBottomWest,BorderLayout.WEST);
+
+    panelEast = new JPanel(new BorderLayout());
+    panelWest = new JPanel(new BorderLayout());
+
+    panelCenter = new JPanel(new BorderLayout());
+    panelCenter.setBackground(Color.red);
+    panelCenter.setOpaque(true);
+
+    panel = new JPanel(new BorderLayout());
+    panel.setPreferredSize(new Dimension(100, 300));
+    panel.add(panelCenter,BorderLayout.CENTER);
+    panel.add(panelEast,BorderLayout.EAST);
+    panel.add(panelWest,BorderLayout.WEST);
+    panel.add(panelBottom,BorderLayout.SOUTH);
+    panel.add(label,BorderLayout.NORTH);
+
+    generateWindowSize();
+    this.addComponentListener(new frameResized());
+
+    this.setContentPane(panel);
+    this.setVisible(true);
+        //this.add(answerLabel,BorderLayout.SOUTH);
+
+    }
+
+    public class frameResized implements ComponentListener {
+
+        @Override
+        public void componentResized(ComponentEvent e) {
+            generateWindowSize();
+        }
+
+        @Override
+        public void componentMoved(ComponentEvent e) {
+            generateWindowSize();
+        }
+
+        @Override
+        public void componentShown(ComponentEvent e) {
+            generateWindowSize();
+        }
+
+        @Override
+        public void componentHidden(ComponentEvent e) {
+            generateWindowSize();
+        }
 
     }
 
@@ -125,6 +265,20 @@ public class MainFrame extends JFrame implements ActionListener {
         }
         if(e.getSource()==thirdButton) {
             if(answerButton == 3) {
+                rightAnswer();
+            } else {
+                wrongAnswer();
+            }
+        }
+        if(e.getSource()==fourthButton) {
+            if(answerButton == 4) {
+                rightAnswer();
+            } else {
+                wrongAnswer();
+            }
+        }
+        if(e.getSource()==fifthButton) {
+            if(answerButton == 5) {
                 rightAnswer();
             } else {
                 wrongAnswer();
